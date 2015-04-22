1c908edc14299b70daaff4b063232020a82a3ef5
Damian Spinek <damian00737@gmail.com>
2015-04-22 18:14:58 +0200

    Test 2 green

--- src/BowlingGameTest.java
+++ src/BowlingGameTest.java
@@ -16,7 +16,7 @@ public class BowlingGameTest {
 	public void testGutterGame(){
 		Game game = new Game();
 		for(int i=0;i<20;i++){
-			game.roll();
+			game.roll(0	);
 		}
 		assertEquals(0, game.score());
 	}
--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,14 @@
 
 public class Game {
 
+	public void roll(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int score() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
