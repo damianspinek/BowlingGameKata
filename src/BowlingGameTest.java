0b9a33deb239c4d0c7cb70b55ebeab5edc330d5c
Damian Spinek <damian00737@gmail.com>
2015-04-22 18:02:57 +0200

    Test zerowy. Przetestowanie czy projekt sie kompiluje

---
+++ src/BowlingGameTest.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.Test;
+
+
+public class BowlingGameTest {
+
+	@Test
+	public void itCompiles() {
+		assertThat(true, equalTo(true));;
+	}
+
+}
